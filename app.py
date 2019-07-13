7f6719be2aedee4d72592c8c23e737a49a97abf9
kozakana <goki727@gmail.com>
2019-07-13 13:12:56 +0900

    split pdf

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 import sys
 import os
+import shutil
 import json
 import requests
 import glob
 from base64 import b64encode
+from pdf2image import convert_from_path
 
 import gcv2hocr
 import hocr_pdf
@@ -14,27 +16,65 @@ MAX_RESULTS = os.environ.get('MAX_RESULTS', 2048)
 API_KEY = os.environ.get('API_KEY')
 PDF_OUTPUT_NAME = os.environ.get('PDF_OUTPUT_NAME')
 
-FILE_DIR = './files'
-PDF_DIR = './pdf'
+TMP_DIR = './tmp'
+INPUT_DIR = './input'
+OUTPUT_DIR = './output'
 
 if API_KEY is None:
     print('API key has not been set')
     sys.exit(1)
 
-file_paths = glob.glob(os.path.join(FILE_DIR, '*'))
-for file_path in file_paths:
-    print(file_path)
-    with open(file_path, 'rb') as f:
-        b64img = b64encode(f.read()).decode()
-        req = {"requests": {'image': {'content': b64img}, 'features': [{'type': 'TEXT_DETECTION', 'maxResults': MAX_RESULTS}], 'imageContext': {'languageHints': LANGUAGE}}}
-        response = requests.post(ENDPOINT_URL, data=json.dumps(req).encode(), params={'key': API_KEY}, headers={'Content-Type': 'application/json'}).json()
-        resp = response['responses'][0] if 'responses' in response and len(response['responses']) >= 0 and "textAnnotations" in response['responses'][0] else False
-        page = gcv2hocr.fromResponse(resp)
+def input_files_and_dirs(paths):
+    origin_pdfs = []
+    images_dirs = []
+    for path in paths:
+        if os.path.isfile(path):
+            _, ext = os.path.splitext(path)
+            if ext.lower() == '.pdf':
+              origin_pdfs.append(path)
+        elif os.path.isdir(path):
+            images_dirs.append(path)
+    return origin_pdfs, images_dirs
 
-        file_name = os.path.splitext(os.path.basename(file_path))[0]
-        hocr = os.path.join(FILE_DIR, (file_name + '.hocr'))
-        with open(hocr, "wb") as f:
-            f.write(page.render().encode('utf-8'))
+def get_images_path(images_dir):
+    files = glob.glob(os.path.join(images_dir, '*'))
+    files.sort()
+    return files
 
-pdf_name = PDF_OUTPUT_NAME or 'out.pdf'
-hocr_pdf.export_pdf(FILE_DIR, 300, pdf_name)
+def ocr_images(images_paths, output_name):
+    path = os.path.dirname(images_paths[0])
+    for images_path in images_paths:
+        with open(images_path, 'rb') as f:
+            b64img = b64encode(f.read()).decode()
+            req = {'requests': {'image': {'content': b64img}, 'features': [{'type': 'TEXT_DETECTION', 'maxResults': MAX_RESULTS}], 'imageContext': {'languageHints': LANGUAGE}}}
+            response = requests.post(ENDPOINT_URL, data=json.dumps(req).encode(), params={'key': API_KEY}, headers={'Content-Type': 'application/json'}).json()
+            resp = response['responses'][0] if 'responses' in response and len(response['responses']) >= 0 and 'textAnnotations' in response['responses'][0] else False
+            page = gcv2hocr.fromResponse(resp)
+
+            file_name = os.path.splitext(os.path.basename(images_path))[0]
+            #hocr = os.path.join(INPUT_DIR, (file_name + '.hocr'))
+            hocr = os.path.join(path, (file_name + '.hocr'))
+            with open(hocr, "wb") as f:
+                f.write(page.render().encode('utf-8'))
+
+    pdf_name = PDF_OUTPUT_NAME or output_name
+    hocr_pdf.export_pdf(path, 300, pdf_name)
+
+def convert_pdf_to_images(pdf_path):
+    tmp_paths = glob.glob(os.path.join(TMP_DIR, '*.jpg'))
+    for tmp_path in tmp_paths:
+        os.remove(tmp_path)
+    images = convert_from_path(pdf_path)
+    for idx, image in enumerate(images):
+        image.save((TMP_DIR + '/tmp{}.jpg').format(idx), 'JPEG')
+    return glob.glob(os.path.join(TMP_DIR, '*.jpg'))
+
+input_paths = glob.glob(os.path.join(INPUT_DIR, '*'))
+pdf_files, images_dirs = input_files_and_dirs(input_paths)
+
+for pdf_file in pdf_files:
+    images_paths = convert_pdf_to_images(pdf_file)
+    ocr_images(images_paths, 'tmp.pdf')
+
+for images_dir in images_dirs:
+    output_name = os.path.basename(images_dir) + '.pdf'
